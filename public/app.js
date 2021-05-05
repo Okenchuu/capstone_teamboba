04ed841c16e4e36cabd32792e0cc934f7ae33378
okenchuu <xz67@uw.edu>
2021-05-05 23:44:13 +0800

    fix position

--- public/app.js
+++ public/app.js
@@ -23,27 +23,27 @@ function getLocation() {
             var city = data.name;
             var country = data.sys.country;
             var rainimg = ['rain','showerrain','raindrop'];
-            var cloudsimg = ['fewclouds','brokenclouds','scatteredclouds','cloudy','cloud'];
+            var cloudsimg = ['fewclouds','brokenclouds','scatteredclouds','cloudy','cloud','cloud1'];
             var skyimg = ['clearsky','sunny'];
             document.getElementById("weather").innerHTML = "<div id='loc'>" + city + ", " + country +  "</div><div id='temp'>" + temp + " °C</div><div id='description'>" + description + "</div>";
 
             if (description.includes('rain') ){
               var input = rainimg[Math.floor(Math.random() * rainimg.length)]
-              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.6)),url(\'img/bgimage/'+input+'.jpg\')';
+              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)),url(\'img/bgimage/'+input+'.jpg\')';
             } else if(description.includes('clouds')){
               var input = cloudsimg[Math.floor(Math.random() * cloudsimg.length)]
-              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.6)),url(\'img/bgimage/'+input+'.jpg\')';
+              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)),url(\'img/bgimage/'+input+'.jpg\')';
             } else if(description.includes('mist')){
-              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.6)),url(\'img/bgimage/mist.jpg\')';
+              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)),url(\'img/bgimage/mist.jpg\')';
             } else if(description.includes('clear')){
               var input = skyimg[Math.floor(Math.random() * skyimg.length)]
-              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.6)),url(\'img/bgimage/clearsky.jpg\')';
+              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)),url(\'img/bgimage/clearsky.jpg\')';
             } else if(description.includes('snow')){
-              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.6)),url(\'img/bgimage/snow.jpg\')';
+              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)),url(\'img/bgimage/snow.jpg\')';
             } else if(description.includes('thunderstorm')){
-              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.6)),url(\'img/bgimage/thunderstorm.jpg\')';
+              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)),url(\'img/bgimage/thunderstorm.jpg\')';
             } else{
-              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.6)),url(\'img/bgimage/else.jpg\')';
+              document.getElementsByClassName("hero-image")[0].style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)),url(\'img/bgimage/else.jpg\')';
             }
         })
 
